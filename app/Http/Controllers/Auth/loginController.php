c802dfe52d18ea3c39dc19e058f89dcdd59907ba
Hantama <pratamahantama@gmail.com>
2020-03-29 21:34:56 +0800

    login controller

--- app/Http/Controllers/Auth/loginController.php
+++ app/Http/Controllers/Auth/loginController.php
@@ -1,3 +1,42 @@
 <?php 
+namespace App\Http\Controllers\Auth;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use DB;
+use Hash;
 
+use App\Functions\checkFunction;
+
+class loginController extends Controller{
+    function index(Request $req){
+        if(checkFunction::checkLogin($req)){
+            return redirect("/");
+        }else{
+            return view("auth.login");
+        }
+    }
+    function login(Request $req){
+        //bcrypt(str);
+        if($req->has("login")){
+            $id=1;
+            $email="asd@asd.asd";
+            $password="$2y$12$ydblUwqOjiRtoZC1TyFgfO6R0l1mqo.e4DG4XyPxtgslo9kfBlvHm";
+            $isCorrect = Hash::check($req->input('password'),$password);
+            if($isCorrect){
+                $loginData = [
+                    "login"=>true,
+                    "id"=>$id
+                ];
+                $req->session()->put(
+                    $loginData
+                );
+            }
+        }
+        if($req->session()->get("login")){
+            return redirect("/");
+        }else{
+            return redirect("/login");
+        }
+    }
+}
 ?>
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/',"dashboardController@index");
+
+Route::post('/login',"Auth.loginController@index");
+Route::get('/login',"Auth.loginController@login");
