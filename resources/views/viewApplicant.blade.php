360a6dc05e068ace36896fa5b8738673c867d164
PutuSenna <kugahunter@gmail.com>
2020-03-31 12:13:35 +0800

    Update viewApplicant.blade.php

---
+++ resources/views/viewApplicant.blade.php
@@ -0,0 +1,264 @@
+@extends('master')
+@section('content')
+
+
+
+    <!-- PAGE WRAP -->
+    <div id="page-wrap">
+        <!-- PRELOADER -->
+        <div class="preloader"></div>
+        <!-- END / PRELOADER -->
+
+        <!-- HEADING PAGE -->
+        <section class="awe-parallax category-heading-section-demo">
+            <div class="awe-overlay"></div>
+            <div class="container">
+                <div class="category-heading-content category-heading-content__2 text-uppercase">
+                  
+                   <H3>Ads space</H3>
+                </div>
+            </div>
+        </section>
+        <!-- END / HEADING PAGE -->
+
+        
+        <section class="filter-page">
+            <div class="container">
+                <h2>Allocattion</h2>
+                <div class="row">
+                    <div class="col-md-10">
+                        <div class="filter-page__content">
+                            <div class="filter-item-wrapper">
+                                <!-- ITEM -->
+                                <div class="flight-item">
+                                   
+                                    <div class="item-body">
+                                        <div class="item-title">
+                                            <h2>
+                                                <a href="flight-detail.html">House 1</a>
+                                            </h2>
+                                        </div>
+                                        <table class="item-table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="route">Username</th>
+                                                    <th class="depart">Available</th>
+                                                    <th class="duration">Rent for</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="route">
+                                                        <ul>
+                                                            {{-- <li>3 <i class="awe-icon awe-icon-arrow-right"></i></li> --}}
+                                                            
+                                                        </ul>
+                                                    </td>
+                                                    <td class="depart">
+                                                        
+                                                        <span class="date">20 March</span>
+                                                    </td>
+                                                    <td class="duration">
+                                                        
+                                                        <span class="date">1 year</span>
+                                                    </td>
+                                                   
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="item-price-more">
+                                        <div class="price">
+                                            <button class="awe-btn">Allocate</button>
+                                            
+                                        </div>
+                                        <button class="awe-btn-1">Reject</button>
+                                    </div>
+                                </div>
+                                <!-- END / ITEM -->
+                                 <!-- ITEM -->
+                                 <div class="flight-item">
+                                   
+                                    <div class="item-body">
+                                        <div class="item-title">
+                                            <h2>
+                                                <a href="flight-detail.html">House 2</a>
+                                            </h2>
+                                        </div>
+                                        <table class="item-table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="route">Unit Available</th>
+                                                    <th class="depart">Available</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="route">
+                                                        <ul>
+                                                            <li>10 <i class="awe-icon awe-icon-arrow-right"></i></li>
+                                                            
+                                                        </ul>
+                                                    </td>
+                                                    <td class="depart">
+                                                        
+                                                        <span class="date">20 March</span>
+                                                    </td>
+                                                   
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="item-price-more">
+                                        <div class="price">
+                                            <span class="amount">$30/m</span>
+                                            
+                                        </div>
+                                        <a href="flight-detail.html" class="awe-btn">Apply</a>
+                                    </div>
+                                </div>
+                                <!-- END / ITEM -->
+                                 <!-- ITEM -->
+                                 <div class="flight-item">
+                                   
+                                    <div class="item-body">
+                                        <div class="item-title">
+                                            <h2>
+                                                <a href="flight-detail.html">House 3</a>
+                                            </h2>
+                                        </div>
+                                        <table class="item-table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="route">Unit Available</th>
+                                                    <th class="depart">Available</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="route">
+                                                        <ul>
+                                                            <li>2 <i class="awe-icon awe-icon-arrow-right"></i></li>
+                                                            
+                                                        </ul>
+                                                    </td>
+                                                    <td class="depart">
+                                                        
+                                                        <span class="date">30 March</span>
+                                                    </td>
+                                                   
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="item-price-more">
+                                        <div class="price">
+                                            <span class="amount">$36/m</span>
+                                            
+                                        </div>
+                                        <a href="flight-detail.html" class="awe-btn">Apply</a>
+                                    </div>
+                                </div>
+                                <!-- END / ITEM -->
+                                 <!-- ITEM -->
+                                 <div class="flight-item">
+                                   
+                                    <div class="item-body">
+                                        <div class="item-title">
+                                            <h2>
+                                                <a href="flight-detail.html">House 4</a>
+                                            </h2>
+                                        </div>
+                                        <table class="item-table">
+                                            <thead>
+                                                <tr>
+                                                    <th class="route">Unit Available</th>
+                                                    <th class="depart">Available</th>
+                                                    
+                                                </tr>
+                                            </thead>
+                                            <tbody>
+                                                <tr>
+                                                    <td class="route">
+                                                        <ul>
+                                                            <li>1 <i class="awe-icon awe-icon-arrow-right"></i></li>
+                                                            
+                                                        </ul>
+                                                    </td>
+                                                    <td class="depart">
+                                                        
+                                                        <span class="date">30 May</span>
+                                                    </td>
+                                                   
+                                                </tr>
+                                               
+                                            </tbody>
+                                        </table>
+                                    </div>
+                                    <div class="item-price-more">
+                                        <div class="price">
+                                            <span class="amount">$20/m</span>
+                                            
+                                        </div>
+                                        <a href="flight-detail.html" class="awe-btn">Apply</a>
+                                    </div>
+                                </div>
+                                <!-- END / ITEM -->
+
+
+                               
+                            </div>
+
+
+                            <!-- PAGINATION -->
+                            <div class="page__pagination">
+                                <span class="pagination-prev"><i class="fa fa-caret-left"></i></span>
+                                <span class="current">1</span>
+                                <a href="#">2</a>
+                                <a href="#">3</a>
+                                <a href="#">4</a>
+                                <a href="#" class="pagination-next"><i class="fa fa-caret-right"></i></a>
+                            </div>
+                            <!-- END / PAGINATION -->
+                        </div>
+                    </div>
+                    <div class="col-md-2">
+                        <div class="page-top" style="margin-left:-6px;">
+                            <select class="awe-select">
+                                <option>Sort by Date</option>
+                                <option>Sort by Price</option>
+                            </select>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
+        </section>
+
+
+
+       
+
+    </div>
+    <!-- END / PAGE WRAP -->
+
+    <!-- LOAD JQUERY -->
+    <script type="text/javascript" src="js/lib/jquery-1.11.2.min.js"></script>
+    <script type="text/javascript" src="js/lib/masonry.pkgd.min.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.parallax-1.1.3.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.owl.carousel.js"></script>
+    <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?v=3"></script>
+    <script type="text/javascript" src="js/lib/md-map-extend.js"></script>
+    <script type="text/javascript" src="js/lib/jquery.magnific-popup.min.js"></script>
+    <script type="text/javascript" src="js/lib/perfect-scrollbar.min.js"></script>
+    <script type='text/javascript' src="js/lib/jquery-ui.js"></script>
+    <script type="text/javascript" src="js/scripts.js"></script>
+
+@endsection
